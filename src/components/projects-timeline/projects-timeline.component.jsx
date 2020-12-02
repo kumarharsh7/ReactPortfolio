f46ef599c2c9d88774e2081cb00d61aed455fa12
Harsh <harshkr.rkl4@gmail.com>
2020-12-02 21:51:48 +0530

    initial commit

---
+++ src/components/projects-timeline/projects-timeline.component.jsx
@@ -0,0 +1,123 @@
+import React from "react";
+import { Timeline, Events, UrlButton, ImageEvent } from "@merc/react-timeline";
+
+// projects
+import quiz_app_img from "../../assets/img/projects/quiz_app-img.jpg";
+import password_generator_img from "../../assets/img/projects/password_generator-img.jpg";
+
+import Accordion from "react-bootstrap/Accordion";
+import Card from "react-bootstrap/Card";
+
+import "./projects-timeline.styles.css";
+
+const TimeLine = () => {
+  return (
+    <div id="projects">
+      <h1 className="pt-3 text-center font-details-b pb-3">PROJECTS</h1>
+      <Timeline>
+        <Events>
+          <ImageEvent
+            className="text-center"
+            text="Quiz App"
+            src={quiz_app_img}
+            alt="Quiz App"
+          >
+            <div className="d-flex justify-content-between flex-column mt-1">
+              <div>
+                <Accordion>
+                  <Card>
+                    <Accordion.Toggle
+                      as={Card.Header}
+                      eventKey="0"
+                      className="p-2 text-center accordian-main"
+                    >
+                      PROJECT DETAILS
+                    </Accordion.Toggle>
+
+                    <Accordion.Collapse eventKey="0" className="text-left">
+                      <Card.Body>
+                        <strong>Description:</strong> This is a quiz app that
+                        displays questions and shows correctly answered
+                        questions based on responses given by user
+                        <hr />
+                      </Card.Body>
+                    </Accordion.Collapse>
+                  </Card>
+                </Accordion>
+              </div>
+              <div className="d-flex justify-content-between flex-nowrap text-center">
+                <UrlButton
+                  href="https://github.com/kumarharsh7/quiz_app/"
+                  target="_blank"
+                >
+                  Github Repo
+                </UrlButton>
+                <UrlButton
+                  href="https://kumarharsh7.github.io/quiz_app"
+                  target="_blank"
+                >
+                  Live Website
+                </UrlButton>
+              </div>
+            </div>
+          </ImageEvent>
+
+          <ImageEvent
+            className="text-center"
+            text="Password Generator"
+            src={password_generator_img}
+            alt=""
+          >
+            <div className="d-flex justify-content-between flex-column mt-1">
+              <div>
+                <Accordion>
+                  <Card>
+                    <Accordion.Toggle
+                      as={Card.Header}
+                      eventKey="0"
+                      className="p-2 text-center accordian-main"
+                    >
+                      PROJECT DETAILS
+                    </Accordion.Toggle>
+
+                    <Accordion.Collapse eventKey="0" className="text-left">
+                      <Card.Body>
+                        <strong>Description:</strong> This web app generates a
+                        random password
+                        {/* <strong>Features:</strong> */}
+                        <ul className="list-styles pt-1">
+                          <li>Ability to decide the length of the password</li>
+                          <li>
+                            Ability to select what your password should entail
+                            (e.g Only UpperCase alphabets,lowercase alphabets
+                            and symbols, e.t.c).
+                          </li>
+                        </ul>
+                      </Card.Body>
+                    </Accordion.Collapse>
+                  </Card>
+                </Accordion>
+              </div>
+              <div className="d-flex justify-content-between flex-nowrap text-center">
+                <UrlButton
+                  href="https://github.com/kumarharsh7/password_generator/"
+                  target="_blank"
+                >
+                  Github Repo
+                </UrlButton>
+                <UrlButton
+                  href="https://kumarharsh7.github.io/password_generator"
+                  target="_blank"
+                >
+                  Live Website
+                </UrlButton>
+              </div>
+            </div>
+          </ImageEvent>
+        </Events>
+      </Timeline>
+    </div>
+  );
+};
+
+export default TimeLine;
---
+++ src/pages/skills/skills-data.js
@@ -0,0 +1,61 @@
+import L_HTML5 from "../../assets/img/skills/html-5.svg";
+import L_CSS3 from "../../assets/img/skills/css3.svg";
+
+import L_REACT from "../../assets/img/skills/react.svg";
+
+import L_MATERIALUI from "../../assets/img/skills/material-ui-1.svg";
+
+import L_GIT from "../../assets/img/skills/git-icon.svg";
+
+import L_GITHUB_PAGES from "../../assets/img/skills/github.svg";
+import L_JAVASCRIPT from "../../assets/img/skills/javascript.svg";
+
+export const skills = {
+  s1: [
+    {
+      link: "https://en.wikipedia.org/wiki/HTML5",
+      imgAltText: "HTML 5",
+      imgSrc: L_HTML5,
+      skillName: "HTML5",
+    },
+    {
+      link:
+        "https://medium.com/beginners-guide-to-mobile-web-development/whats-new-in-css-3-dcd7fa6122e1",
+      imgAltText: "CSS 3",
+      imgSrc: L_CSS3,
+      skillName: "CSS3",
+    },
+    {
+      link: "https://www.javascript.com/",
+      imgAltText: "JavaScript",
+      imgSrc: L_JAVASCRIPT,
+      skillName: "JavaScript",
+    },
+
+    {
+      link: "https://reactjs.org/",
+      imgAltText: "React JS",
+      imgSrc: L_REACT,
+      skillName: "React JS",
+    },
+
+    {
+      link: "https://material-ui.com/",
+      imgAltText: "Material-UI",
+      imgSrc: L_MATERIALUI,
+      skillName: "Material-UI",
+    },
+    {
+      link: "https://pages.github.com/",
+      imgAltText: "GitHub",
+      imgSrc: L_GITHUB_PAGES,
+      skillName: "GitHub",
+    },
+    {
+      link: "https://git-scm.com/",
+      imgAltText: "GIT",
+      imgSrc: L_GIT,
+      skillName: "GIT",
+    },
+  ],
+};
