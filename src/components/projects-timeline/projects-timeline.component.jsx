66fc8c17d8ecbf6c915e59080e66f8d4521d361c
Harsh <kumarharsh7620@gmail.com>
2020-12-18 00:04:41 +0530

    added project

--- src/components/projects-timeline/projects-timeline.component.jsx
+++ src/components/projects-timeline/projects-timeline.component.jsx
@@ -3,6 +3,7 @@ import { Timeline, Events, UrlButton, ImageEvent } from "@merc/react-timeline";
 
 // projects
 import quiz_app_img from "../../assets/img/projects/quiz_app-img.jpg";
+import TechHire_img from "../../assets/img/projects/TechHire-img.jpg";
 import password_generator_img from "../../assets/img/projects/password_generator-img.jpg";
 
 import Accordion from "react-bootstrap/Accordion";
@@ -16,6 +17,46 @@ const TimeLine = () => {
       <h1 className="pt-3 text-center font-details-b pb-3">PROJECTS</h1>
       <Timeline>
         <Events>
+          <ImageEvent
+            className="text-center"
+            text="Tech Hire"
+            src={TechHire_img}
+          >
+            <div className="d-flex justify-content-between flex-column mt-1">
+              <div>
+                <Accordion>
+                  <Card>
+                    <Accordion.Toggle
+                      as={Card.Header}
+                      eventKey="0"
+                      className="p-2 text-center accordian-main"
+                    >
+                      PROJECT DETAILS
+                    </Accordion.Toggle>
+
+                    <Accordion.Collapse eventKey="0" className="text-left">
+                      <Card.Body>
+                        <strong>Description:</strong> A react job portal . It
+                        fetches jobs data from Github Jobs API
+                        <hr />
+                      </Card.Body>
+                    </Accordion.Collapse>
+                  </Card>
+                </Accordion>
+              </div>
+              <div className="d-flex justify-content-between flex-nowrap text-center">
+                <UrlButton
+                  href="https://github.com/kumarharsh7/TechHire/"
+                  target="_blank"
+                >
+                  Github Repo
+                </UrlButton>
+                <UrlButton href="https://techhire.netlify.app" target="_blank">
+                  Live Website
+                </UrlButton>
+              </div>
+            </div>
+          </ImageEvent>
           <ImageEvent
             className="text-center"
             text="Quiz App"
